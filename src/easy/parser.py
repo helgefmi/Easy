cce5efde98f406e16c139cdd2e91680b0f3df127
Helge Milde <helge@monsternett.no>
2011-03-02 21:53:58 +0100

    Made the parser a bit more flexible

--- src/easy/parser.py
+++ src/easy/parser.py
@@ -4,6 +4,14 @@ class Parser(object):
     def __init__(self, tokens):
         self.tokens = tokens
     
+    def _next_tokens(self, *args):
+        if len(args) > len(self.tokens):
+            return False
+        for i, arg in enumerate(args):
+            if self.tokens[i].type != arg:
+                return False
+        return True
+
     def _eat_token(self, what):
         token, self.tokens = self.tokens[0], self.tokens[1:]
         assert token.type == what, "Expected %s, got %s." % (what, token.type)
@@ -24,62 +32,52 @@ class Parser(object):
         return True
 
     def parse_expression(self):
-        return self.parse_string() or self.parse_identifier() \
-                or self.parse_number()
+        return self.parse_string() or self.parse_number() or self.parse_funccall()
+
+    def parse_number(self):
+        token = self._eat_if_token('tok_number')
+        return NumberExpr(token.value) if token else False
+
+    def parse_string(self):
+        token = self._eat_if_token('tok_string')
+        return StringExpr(token.value) if token else False
+
+    def parse_function(self):
+        if not self._eat_if_token('tok_def'):
+            return False
+        
+        func_name = self._eat_token('tok_identifier').value
+
+        args = []
+        if self._eat_if_token('tok_paren_start'):
+            while self._curtype() == 'tok_identifier':
+                args.append(self._eat_token('tok_identifier').value)
+            self._eat_token('tok_paren_end')
+
+        block = self.parse_block()
+        return FuncDefinition(func_name, args, block)
 
     def parse_block(self):
         block = []
         self._eat_token('tok_do')
         while True:
             expr = self.parse_expression()
+            assert expr, map(str, self.tokens)[:10]
             self._eat_token('tok_semicolon')
-            assert expr
             expr = ExprStatement(expr)
             block.append(expr)
             if self._eat_if_token('tok_end'):
                 break
         return BlockStatement(block)
 
-    def parse_function(self):
-        token = self._eat_token('tok_def')
-        if not token:
+    def parse_funccall(self):
+        if not self._next_tokens('tok_identifier', 'tok_paren_start'):
             return False
-        
-        func_name = self.parse_identifier(parse_funccall=False)
 
+        func_name = self._eat_token('tok_identifier')
+        self._eat_token('tok_paren_start')
         args = []
-        if self._eat_if_token('tok_paren_start'):
-            while self._curtype() != 'tok_paren_end':
-                args.append(self.parse_identifier(parse_funccall=False))
-            self._eat_token('tok_paren_end')
-
-        block = self.parse_block()
-
-        return FuncDefinition(func_name, args, block)
-
-    def parse_number(self):
-        number = self._eat_if_token('tok_number')
-        if not number:
-            return False
-        return NumberExpr(number.value)
-
-    def parse_string(self):
-        string = self._eat_if_token('tok_string')
-        if not string:
-            return False
-        return StringExpr(string.value)
-
-    def parse_identifier(self, parse_funccall=True):
-        id = self._eat_if_token('tok_identifier')
-        if not id:
-            return False
-
-        if not parse_funccall:
-            return id
-
-        if self._eat_if_token('tok_paren_start'):
-            args = []
-            while self._curtype() != 'tok_paren_end':
-                args.append(self.parse_expression())
-            self._eat_token('tok_paren_end')
-            return FuncCallExpr(id, args)
+        while self._curtype() != 'tok_paren_end':
+            args.append(self.parse_expression())
+        self._eat_token('tok_paren_end')
+        return FuncCallExpr(func_name, args)
--- src/easy/visitors/codegen.py
+++ src/easy/visitors/codegen.py
@@ -15,12 +15,10 @@ class RegisterStack(object):
         return reg
 
     def push(self, item=None):
-        if item is not None:
-            self._stack.append(item)
-        else:
-            reg = self._free_regs.pop()
-            self._stack.append(reg)
-        return self._stack[-1]
+        if item is None:
+            item = self._free_regs.pop()
+        self._stack.append(item)
+        return item
 
     def save_caller_regs(self, codegen):
         regs = [reg for reg in self._caller_save if reg not in self._free_regs]
@@ -36,7 +34,7 @@ class CodeGenVisitor(BaseVisitor):
     def __init__(self, *args, **kwargs):
         super(CodeGenVisitor, self).__init__(*args, **kwargs)
         self.output = ''
-        self.reg_stack = RegisterStack()
+        self.regs = RegisterStack()
         self._string_labels = {}
 
     def _get_string_label(self, string):
@@ -52,50 +50,42 @@ class CodeGenVisitor(BaseVisitor):
             self.omit('.%s:' % label)
             self.omit('.string "%s"' % string)
 
-    def _omit_footer(self):
-        self.omit('')
-        self.omit('')
-
     def omit(self, line):
-        if not isinstance(line, str):
-            for l in line:
-                self.omit(l)
-            return
-
         if line and line[-1] != ':' and sum(1 for x in ('.globl',) if line.startswith(x)) == 0:
             line = '\t' + line
         self.output += line + '\n'
 
     def visitNumberExpr(self, node):
-        self.reg_stack.push('$%d' % node.number)
+        self.regs.push('$%d' % node.number)
 
     def visitStringExpr(self, node):
         label = self._get_string_label(node.string)
-        #reg = self.reg_stack.push()
+        #reg = self.regs.push()
         #self.omit('movl $.%s, %s' % (label, reg))
-        self.reg_stack.push('$.%s' % label)
+        self.regs.push('$.%s' % label)
 
     def visitFuncCallExpr(self, node):
-        regs_saved = self.reg_stack.save_caller_regs(self)
+        regs_saved = self.regs.save_caller_regs(self)
 
         self._visit_list(node.args)
 
         for arg in node.args:
-            loc = self.reg_stack.pop()
+            loc = self.regs.pop()
             self.omit('pushl %s' % loc)
 
         self.omit('call %s' % node.func_name)
-        self.omit('addl $%d, %%esp' % (len(node.args) * 4))
+        if node.args:
+            self.omit('addl $%d, %%esp' % (len(node.args) * 4))
 
-        dst = self.reg_stack.push()
+        dst = self.regs.push()
         if dst != '%eax':
             self.omit('movl %%eax, %s' % dst)
 
-        self.reg_stack.pop_regs(self, regs_saved)
+        self.regs.pop_regs(self, regs_saved)
 
     def visitExprStatement(self, node):
         self.visit(node.expr)
-        self.reg_stack.pop()
+        self.regs.pop()
 
     def visitBlockStatement(self, node):
         self._visit_list(node.block)
