b569e8812463f447c2923975633faee1c0c83dcc
Helge Milde <helge@monsternett.no>
2011-03-03 21:00:36 +0100

    Removed some unused overhead

--- src/easy/visitors/constant_folding.py
+++ src/easy/visitors/constant_folding.py
@@ -2,13 +2,6 @@ from easy.ast import NumberExpr
 from easy.visitors.base import BaseVisitor
 
 class ConstantFoldingVisitor(BaseVisitor):
-    def _assert_has_parent(self, parent, *args):
-        for node in args:
-            if isinstance(node, list):
-                self._assert_has_parent(parent, *node)
-            elif node is not None:
-                assert node.parent == parent
-
     def visitNumberExpr(self, node):
         return NumberExpr(node.number)
 
@@ -16,7 +9,6 @@ class ConstantFoldingVisitor(BaseVisitor):
         return NumberExpr(node.string)
 
     def visitFuncCallExpr(self, node):
-        self._assert_has_parent(node, node.args)
         constants = self._visit_list(node.args)
 
         for i, constant in enumerate(constants):
@@ -27,26 +19,18 @@ class ConstantFoldingVisitor(BaseVisitor):
             node.args[i] = constant
 
     def visitExprStatement(self, node):
-        self._assert_has_parent(node, node.expr)
-        constant = self.visit(node.expr)
-
-        # TODO: Make a Noop AstNode?
+        return self.visit(node.expr)
 
     def visitBlockStatement(self, node):
-        self._assert_has_parent(node, node.block)
         self._visit_list(node.block)
 
     def visitTopLevel(self, node):
-        self._assert_has_parent(node, node.block)
         self._visit_list(node.block)
 
     def visitFuncDefinition(self, node):
-        self._assert_has_parent(node, node.block)
         self.visit(node.block)
 
     def visitIfStatement(self, node):
-        self._assert_has_parent(node, node.cond, node.true_block,
-                                node.false_block)
         constant = self.visit(node.cond)
         if not (constant is None or constant.is_leaf_node()):
             node.cond = constant
@@ -56,7 +40,6 @@ class ConstantFoldingVisitor(BaseVisitor):
             self.visit(node.false_block)
 
     def visitBinaryOpExpr(self, node):
-        self._assert_has_parent(node, node.lhs, node.rhs)
         lconstant = self.visit(node.lhs)
         if not (lconstant is None or lconstant.is_leaf_node()):
             node.lhs = lconstant
